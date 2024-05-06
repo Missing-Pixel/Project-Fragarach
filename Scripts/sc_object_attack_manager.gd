65d7a3809fa60a5e4b303cb9e55eebc06bfd42e3
TraveenW <traveenw@gmail.com>
2024-05-06 15:10:28 +1000

    added Object Attack Manager script

---
+++ Scripts/sc_object_attack_manager.gd
@@ -0,0 +1,15 @@
+class_name ObjectAttackManager
+extends Node
+## Manages the queuing of attacks, and executing their animations
+## Recieves hitbox data from a signal and sends it to the target
+
+var attack_queue: Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
