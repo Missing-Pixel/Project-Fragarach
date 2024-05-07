adc72f5ea76f327141dc3c0262404de7d6baae66
TraveenW <traveenw@gmail.com>
2024-05-07 12:26:49 +1000

    Added character health script

--- Scripts/sc_character_health.gd
+++ Scripts/sc_character_health.gd
@@ -1,12 +1,5 @@
-class_name InputManager
 extends Node
-## Sends signals out based on user input
 
-
-# Input signals in game state: LEVEL
-signal inputted_move_player(x_vel, y_vel)
-signal inputted_select_card(card_slot)
-signal inputted_start_combo()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Scripts/sc_player_states.gd
+++ Scripts/sc_player_states.gd
@@ -2,27 +2,25 @@ class_name PlayerStates
 extends CharacterStates
 ## Variant of CharacterStates, meant for managing inputs for the Player
 
-signal moved_player(velocity)
-signal selected_card(card_slot_value)
-signal started_combo()
+# The following signals would need to be manually connected to noted scripts
+signal moved_player(velocity) # Movement
+signal selected_card(card_slot_value) # CardManager
+signal started_combo() # CardManager
 
 var input_velocity: Array = [0, 0]
 
 # Relay move player signal to movement script
-# Input applicable in IDLE and MOVING states
 func _on_input_move(x, y):
 	if (current_state == States.IDLE or current_state == States.MOVING):
 		input_velocity = [x, y]
 		moved_player.emit(input_velocity)
 
 # Relay select card signal to card management script
-# Input applicable in IDLE and MOVING states
 func _on_input_select_card(card_slot):
 	if (current_state == States.IDLE or current_state == States.MOVING):
 		selected_card.emit(card_slot)
 
 # Relay initiating combo to card management script
-# Input applicable in IDLE and MOVING states
 func _on_input_start_combo():
 	if (current_state == States.IDLE or current_state == States.MOVING):
 			started_combo.emit()
@@ -47,7 +45,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# Switches between IDLE and MOVING states depending on whether player is moving
+	# Switches between IDLE and MOVING states depending on whether player moves
 	if (current_state == States.IDLE and not _check_all_zero(input_velocity)):
 		current_state = States.MOVING
 	elif (current_state == States.MOVING and _check_all_zero(input_velocity)):
