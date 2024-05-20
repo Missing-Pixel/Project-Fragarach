7d9291d6322b08a3a598db04e37924130bacd490
TraveenW <traveenw@gmail.com>
2024-05-20 15:43:59 +1000

    Added menu control functionality; duplicated text level

--- Scripts/Autoloaded/sc_input_manager.gd
+++ Scripts/Autoloaded/sc_input_manager.gd
@@ -3,19 +3,28 @@ extends Node
 ## Sends signals out based on user input
 ## Signal recipients will connect to InputManager through code running in _ready() 
 
-enum GameState { MENU=0, LEVEL=1, DEBUG=99}
+enum GameState { MENU=0, LEVEL=1 }
+
+# Input signals for game state: MENU
+signal inputted_move_cursor(x, y)
+signal inputted_confirm_menu()
+signal inputted_cancel_menu()
 
 # Input signals for game state: LEVEL
 signal inputted_move_player(x, y)
 signal inputted_select_card(card_slot)
 signal inputted_start_combo()
+signal inputted_pause_game()
 
 @export var game_state: GameState = GameState.LEVEL
-@export_group("DEBUG")
-@export var check_move: bool = false
-@export var check_buttons: bool = false
+@export var menu_move_cooldown: float = 0.25
 
 var is_moving: bool = false
+var move_timer: float = 0
+
+# Switch game states
+func switch_game_states(new_state):
+	game_state = new_state
 
 # Emit signal for controlling player movement
 func _emit_level_move_player():
@@ -30,8 +39,8 @@ func _emit_level_move_player():
 		inputted_move_player.emit(0, 0)
 		is_moving = false
 
-# Emit signals for card management controls
-func _emit_level_card_management():
+# Emit signals for level controls
+func _emit_level_controls():
 	if Input.is_action_just_pressed("Select Card 1"):
 		inputted_select_card.emit(1)
 	if Input.is_action_just_pressed("Select Card 2"):
@@ -43,38 +52,53 @@ func _emit_level_card_management():
 	
 	if Input.is_action_just_pressed("Start Combo"):
 		inputted_start_combo.emit()
-	pass
+	if Input.is_action_just_pressed("Pause Game"):
+		inputted_pause_game.emit()
 
-# Switch game states
-func switch_game_states(new_state):
-	game_state = new_state
+# Reduces the movement axes to 4 cardinal directions, and emit if cooldown allows it
+func _emit_menu_move_cursor():
+	var cursor_vel: Vector2 = Vector2(0, 0)
+	var vel_angle: float
+	
+	cursor_vel.x = Input.get_axis("Move Left", "Move Right")
+	cursor_vel.y = Input.get_axis("Move Up", "Move Down")
+	
+	if not(cursor_vel.x == 0 and cursor_vel.y == 0) and (move_timer >= menu_move_cooldown):
+		move_timer = 0
+		vel_angle = _get_simple_angle_degrees(cursor_vel)
+		
+		# Checks angle of vector, and emits signal based on angle
+		if (vel_angle <= 45) or (vel_angle > 315): # Right
+			inputted_move_cursor.emit(1, 0)
+		elif (vel_angle <= 135) or (vel_angle > 45): # Down
+			inputted_move_cursor.emit(0, 1)
+		elif (vel_angle <= 225) or (vel_angle > 135): # Left
+			inputted_move_cursor.emit(-1, 0)
+		if (vel_angle <= 315) or (vel_angle > 225): # Up
+			inputted_move_cursor.emit(0, -1)
+
+# Emits menu controls
+func _emit_menu_controls():
+	if Input.is_action_just_pressed("Confirm Menu"):
+		inputted_confirm_menu.emit()
+	if Input.is_action_just_pressed("Cancel Menu"):
+		inputted_cancel_menu.emit()
+
+# Returns angle in degrees between 0 (inclusive) and 360 (exclusive)
+func _get_simple_angle_degrees(input_vel: Vector2):
+	var angle_rad = input_vel.angle()
+	var angle_deg = angle_rad * 180/PI
+	angle_deg = angle_deg % 360
+	return angle_deg
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (game_state == GameState.LEVEL or game_state == GameState.DEBUG):
+	if (game_state == GameState.LEVEL):
 		_emit_level_move_player()
-		_emit_level_card_management()
+		_emit_level_controls()
+	elif (game_state == GameState.MENU):
+		_emit_menu_move_cursor()
+		_emit_menu_controls()
 	
-	# -------------------------------------------------------------------------
-	# DEBUG
-	_debug_check_bind()
-
-# -------------------------------------------------------------------------------------------------
-# DEBUG SECTION 
-signal started_debug()
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	if (game_state == GameState.DEBUG):
-		started_debug.emit()
-# Debug: print pressed bind
-func _debug_check_bind():
-	if Input.is_action_just_pressed("Select Card 1"):
-		print("Select Card 1", " pressed")
-	if Input.is_action_just_pressed("Select Card 2"):
-		print("Select Card 2", " pressed")
-	if Input.is_action_just_pressed("Select Card 3"):
-		print("Select Card 3", " pressed")
-	if Input.is_action_just_pressed("Select Card 4"):
-		print("Select Card 4", " pressed")
-	if Input.is_action_just_pressed("Start Combo"):
-		print("Start Combo", " pressed")
+	if (move_timer < menu_move_cooldown):
+		move_timer += delta
